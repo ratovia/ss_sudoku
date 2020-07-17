a3b3d58ed99b9b87284c5c086fc61e740c588bad
ratovia <ratovia@hotmail.co.jp>
2020-07-17 18:05:26 +0900

    puzzle index page

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,17 +1,15 @@
 class Api::V1::PuzzlesController < ApplicationController
   def index
     @puzzles = Puzzle.all
-    question_array = []
-    @puzzles.each do |puzzle|
-      array = []
-      9.times do |i|
-        array << puzzle.question.slice(0*i,9).split("").map(&:to_i)
-      end
-      question_array << array
-    end
-    render json: {question_array: question_array}
+    render json: @puzzles
   end
 
   def show
+    puzzle = Puzzle.find(params[:id])
+    puzzle_array = []
+    9.times do |i|
+      puzzle_array << puzzle.question.slice(i*9,9).split("").map(&:to_i)
+    end
+    render json: puzzle_array
   end
 end
